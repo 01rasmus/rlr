36fa388e06a6557f9dfab0aa44aaf6bc9e756ab5
01rasmus <68335712+01rasmus@users.noreply.github.com>
2026-06-25 00:18:56 +0200

    fix: default texture array size

--- src/rlr/resources/texture.c
+++ src/rlr/resources/texture.c
@@ -67,7 +67,7 @@ rlr_res_texture_t* rlr_res_texture_default() {
     texture->height = 1;
     texture->width = 1;
 
-    uint8_t data[1] = {255, 255, 255};
+    uint8_t data[3] = {255, 255, 255};
     texture->texture = rlr_backend()->create_nearest_texture(data, 1, 1, 3, false);
     if(texture->texture == 0) {
         rlr_error_set(RLR_ERR_BACKEND_NULL_HANDLE);
